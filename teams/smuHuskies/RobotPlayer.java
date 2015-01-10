c27b4b6d11ec39e8360aebe6818b0b81f4fce5aa
Matt Triff <matt.triff@gmail.com>
2015-01-09 20:15:28 -0500

    Hole protection prototype completed

--- teams/smuHuskies/RobotPlayer.java
+++ teams/smuHuskies/RobotPlayer.java
@@ -13,9 +13,8 @@ public class RobotPlayer {
 	static int myRange;
 	
 	//Detect holes
-	static int minX;
-	static int minY;
-	static MapLocation[][] towerMap;
+	static int TOWER_HOLES_BEGIN = 2000;
+	static int NUM_HANDWASH_STATIONS_BUILT = 0;
 	
 	// See shamelessly borrowed functions at bottom of file
 	static Direction[] directions = {Direction.NORTH, Direction.NORTH_EAST, Direction.EAST, Direction.SOUTH_EAST, Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST};
@@ -25,6 +24,7 @@ public class RobotPlayer {
 	
 	// Defence
 	static int NUM_TOWER_PROTECTORS = 10;
+	static int NUM_HOLE_PROTECTORS = 2;
 	static int PROTECT_OTHERS_RANGE = 10;
 	
 	public static void run(RobotController incomingRc) {
@@ -78,11 +78,12 @@ public class RobotPlayer {
 		if (rc.isCoreReady()) {
 			defend();
 			try {
-				int numHandWashStations = rc.readBroadcast(0);
+				int numHandWashStations = rc.readBroadcast(NUM_HANDWASH_STATIONS_BUILT);
 				if (numHandWashStations < 1 && rc.isCoreReady()) {
 					boolean successful = tryBuild(Direction.NORTH, RobotType.HANDWASHSTATION);
 					if (successful) {
-						rc.broadcast(0, numHandWashStations++);
+						numHandWashStations++;
+						rc.broadcast(NUM_HANDWASH_STATIONS_BUILT, numHandWashStations);
 					}
 				}
 			} catch (GameActionException e) {
@@ -106,12 +107,12 @@ public class RobotPlayer {
 			}
 			return;
 		}
-		
 		// A2, Protect Nearby
 		RobotInfo[] engagedRobots = getRobotsEngagedInAttack();
 		if(engagedRobots != null && engagedRobots.length>0) { // Check broadcasts for enemies that are being attacked
 			// TODO: Calculate which enemy is attacking/within range/closest to teammate
 			// For now, just picking the first enemy
+			// Once our unit is in range of the other unit, A1 will takeover
 			for (RobotInfo robot : engagedRobots) {
 				if (robot.team == enemyTeam) {
 					Direction[] directions = getDirectionsToward(robot.location);
@@ -129,16 +130,40 @@ public class RobotPlayer {
 					}
 				}
 			}
-			// Once our unit is in range of the other unit, A1 will takeover
 		}
-		
-		// Check broadcast for holes
-		if(false) {
+		int towerHoleX = -1;
+		try {
+			towerHoleX = rc.readBroadcast(TOWER_HOLES_BEGIN);
+		} catch (GameActionException e1) {
+			e1.printStackTrace();
+		}
+		boolean defendingHole = false;
+		if(towerHoleX != -1) {
 			// B1, Protect Holes From Other Towers
 			// Holes to be computed once, shared in broadcast
-			
 			// Go to first hole with < threshold units there, or, pick one at random
-		} else if(true) {
+			int towerHolesIndex = TOWER_HOLES_BEGIN;
+			int towerHoleY;
+			do {
+				try {
+					towerHoleX = rc.readBroadcast(towerHolesIndex);
+					towerHolesIndex++;
+					towerHoleY = rc.readBroadcast(towerHolesIndex);
+					towerHolesIndex++;
+					if (towerHoleX != -1) {
+						MapLocation holeLocation = new MapLocation(towerHoleX, towerHoleY);
+						RobotInfo[] nearbyTeammates = rc.senseNearbyRobots(holeLocation, 5, myTeam);
+						if (nearbyTeammates.length < NUM_HOLE_PROTECTORS) {
+							defendingHole = true;
+							goToLocation(holeLocation);
+							towerHoleX = -1;
+						}
+					}
+				} catch (GameActionException e) {
+					e.printStackTrace();
+				}
+			} while(towerHoleX != -1);
+		} else if(!defendingHole) {
 			// B2, Protect Towers
 			// TODO: Compute in advance at HQ, check Broadcasts for tower that needs units
 			MapLocation[] myTowers = rc.senseTowerLocations();
@@ -155,19 +180,7 @@ public class RobotPlayer {
 				}
 			}
 			// TODO: End compute
-			Direction[] directions = getDirectionsToward(closestTower);
-			for (Direction d : directions) {
-				if (rc.isCoreReady()) {
-					if(rc.canMove(d)) {
-						try {
-							rc.move(d);
-						} catch (GameActionException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-				rc.yield();
-			}
+			goToLocation(closestTower);
 		} else if(false) {
 			// B3, Protect Other Buildings
 			// TBD
@@ -181,6 +194,22 @@ public class RobotPlayer {
 	 */
 	public static RobotInfo[] getEnemiesInAttackRange() {
 		return rc.senseNearbyRobots(myRange, enemyTeam);
+	}
+	
+	public static void goToLocation(MapLocation location) {
+		Direction[] directions = getDirectionsToward(location);
+		for (Direction d : directions) {
+			if (rc.isCoreReady()) {
+				if(rc.canMove(d)) {
+					try {
+						rc.move(d);
+					} catch (GameActionException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			rc.yield();
+		}
 	}
 	
 	public static RobotInfo[] getRobotsEngagedInAttack() {
@@ -223,8 +252,13 @@ public class RobotPlayer {
 		}
 		// Naively say, if overlapping by two towers, there is no path
 		int[] overlapped = new int[towerRadii.length];
+		int holesBroadcastIndex = TOWER_HOLES_BEGIN;
 		for(int i = 0; i<towerRadii.length; i++) {
 			MapLocation[] locations = towerRadii[i];
+			boolean coveredLeft = false;
+			boolean coveredRight = false;
+			boolean coveredTop = false;
+			boolean coveredBottom = false;
 			for (int j = 0; j < towerRadii.length; j++) {
 				if (j != i) {
 					MapLocation[] otherLocations = towerRadii[j];
@@ -233,24 +267,79 @@ public class RobotPlayer {
 							locations[0].y <= otherLocations[otherLocations.length-1].y &&
 							otherLocations[0].y <= locations[locations.length-1].y) {
 						overlapped[i]++;
+						Direction otherTowerDir = towerLocations[i].directionTo(towerLocations[j]);
+						if (otherTowerDir.equals(Direction.EAST) || otherTowerDir.equals(Direction.NORTH_EAST) || otherTowerDir.equals(Direction.SOUTH_EAST)) {
+							coveredLeft = true;
+						}
+						if (otherTowerDir.equals(Direction.WEST) || otherTowerDir.equals(Direction.NORTH_WEST) || otherTowerDir.equals(Direction.SOUTH_WEST)) {
+							coveredRight = true;
+						}
+						if (otherTowerDir.equals(Direction.NORTH) || otherTowerDir.equals(Direction.NORTH_EAST) || otherTowerDir.equals(Direction.NORTH_WEST)) {
+							coveredTop = true;
+						}
+						if (otherTowerDir.equals(Direction.SOUTH) || otherTowerDir.equals(Direction.SOUTH_EAST) || otherTowerDir.equals(Direction.SOUTH_WEST)) {
+							coveredBottom = true;
+						}
 					}
 				}
 			}
-			if(overlapped[i]<2 && !rc.isPathable(RobotType.DRONE, new MapLocation(locations[0].x - 1, locations[0].y))) {
+			// TODO: Account for walls
+			if(overlapped[i]<2 && !rc.isPathable(RobotType.BEAVER, new MapLocation(locations[0].x - 1, locations[0].y))) {
 				overlapped[i]++;
+				coveredLeft = true;
 			}
-			if(overlapped[i]<2 && !rc.isPathable(RobotType.DRONE, new MapLocation(locations[locations.length-1].x + 1, locations[0].y))) {
+			if(overlapped[i]<2 && !rc.isPathable(RobotType.BEAVER, new MapLocation(locations[locations.length-1].x + 1, locations[0].y))) {
 				overlapped[i]++;
+				coveredRight = true;
 			}
-			if(overlapped[i]<2 && !rc.isPathable(RobotType.DRONE, new MapLocation(locations[0].x, locations[0].y - 1))) {
+			if(overlapped[i]<2 && !rc.isPathable(RobotType.BEAVER, new MapLocation(locations[0].x, locations[0].y - 1))) {
 				overlapped[i]++;
+				coveredTop = true;
 			}
-			if(overlapped[i]<2 && !rc.isPathable(RobotType.DRONE, new MapLocation(locations[0].x, locations[locations.length-1].y + 1))) {
+			if(overlapped[i]<2 && !rc.isPathable(RobotType.BEAVER, new MapLocation(locations[0].x, locations[locations.length-1].y + 1))) {
 				overlapped[i]++;
+				coveredBottom = true;
 			}
 //			System.out.println("Tower " + i + " overlapped " + overlapped[i] + " " + towerLocations[i]);
+			if (overlapped[i] < 2) {
+				try {
+					int towerAttackRadius = (int) Math.sqrt(RobotType.TOWER.attackRadiusSquared) + 1;
+					if (!coveredLeft) {
+						System.out.println("Tower " + towerLocations[i] + " Not covered left");
+						rc.broadcast(holesBroadcastIndex, towerLocations[i].x - towerAttackRadius);
+						rc.broadcast(holesBroadcastIndex + 1, towerLocations[i].y);
+						holesBroadcastIndex+=2;
+					}
+					if (!coveredRight) {
+						System.out.println("Tower " + towerLocations[i] + " Not covered right");
+						rc.broadcast(holesBroadcastIndex, towerLocations[i].x + towerAttackRadius);
+						rc.broadcast(holesBroadcastIndex + 1, towerLocations[i].y);
+						holesBroadcastIndex+=2;
+					}
+					if (!coveredTop) {
+						System.out.println("Tower " + towerLocations[i] + " Not covered top");
+						rc.broadcast(holesBroadcastIndex, towerLocations[i].x);
+						rc.broadcast(holesBroadcastIndex + 1, towerLocations[i].y - towerAttackRadius);
+						holesBroadcastIndex+=2;
+					}
+					if (!coveredBottom) {
+						System.out.println("Tower " + towerLocations[i] + " Not covered bottom");
+						rc.broadcast(holesBroadcastIndex, towerLocations[i].x);
+						rc.broadcast(holesBroadcastIndex + 1, towerLocations[i].y + towerAttackRadius);
+						holesBroadcastIndex+=2;
+					}
+				} catch (GameActionException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-		// Broadcast towers with holes
+		// Signify end of holes
+		try {
+			rc.broadcast(holesBroadcastIndex, -1);
+		} catch (GameActionException e) {
+			e.printStackTrace();
+		}
+		
 		System.out.println("BYTEEND on " + Clock.getRoundNum() + ": " + Clock.getBytecodeNum());
 		return null;
 	}
